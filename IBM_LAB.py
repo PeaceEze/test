14c4a8f215930eef3ccdd13fafffc13a950027e0
PeaceEze <105740201+PeaceEze@users.noreply.github.com>
2022-09-28 10:50:08 -0500

    Add files via upload

---
+++ IBM_LAB.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Peace Eze**
+# <br>
+# Data Scientist
+
+# *I am interested in data science because I love gathering insights from data*
+
+# ### My code in cell 5 is to add 1 and 2 
+
+# In[5]:
+
+
+1 + 2 
+
+
+# Grocery list
+# - carrot
+# - ~egg~
+# - peanuts 
+#  
+# Best buy list
+# 1. iphone charger 
+# 2. Earphones 
+
+# In[ ]:
+
+
+
+
